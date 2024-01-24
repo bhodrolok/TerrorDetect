6e0c7cd4fca7cc26f2d5e5112374c88a572566e3
RC <51386657+bhodrolok@users.noreply.github.com>
2024-01-24 19:53:02 +0530

    Update combiner.py

--- combiner.py
+++ combiner.py
@@ -1,17 +1,14 @@
 import snscrape.modules.twitter as sntwitter
 import pandas as pd
-import os
 import glob
 
-a = os.getcwd()
+file_extension = 'csv'
+# Use list comprehension to get list of filenames that have .csv file extension
+all_csv_filenames = [i for i in glob.glob('*.{}'.format(file_extension))]
+# Combine these files into a single list
+combined_csv_files = pd.concat([pd.read_csv(f) for f in all_csv_filenames])
+# Export to a final CSV dataset
+combined_csv_files.to_csv("dataset.csv", index=False)
 
-extension = 'csv'
-all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
-
-#combine all files in the list
-combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames ])
-#export to csv
-combined_csv.to_csv("dataset.csv", index=False)
-
-#References:
-#https://www.freecodecamp.org/news/how-to-combine-multiple-csv-files-with-8-lines-of-code-265183e0854/
+# References:
+# https://www.freecodecamp.org/news/how-to-combine-multiple-csv-files-with-8-lines-of-code-265183e0854/
